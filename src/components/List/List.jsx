0d4eaac8b8ec02de39a23128b26716449089ecd7
Kooberium <kooberium@gmail.com>
2023-06-08 22:29:16 +0300

    Added temporary fake data for list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,15 +3,20 @@ import React from "react";
 import styles from "./list.module.css";
 
 const List = (props) => {
+  const fake = []
+  for (let i = 0; i < 50; i++) {
+    fake.push({name: `name - ${i}`});
+  };
   return (
     <ul className={styles.list}>
-        {props.data.map((el, i) => <li className={styles.list_item} key={i}>
+        {/* {props.data.map((el, i) => <li className={styles.list_item} key={i}>
           <p>{el.name}</p>
           <div>
             <p>*</p>
-            
+
           </div>
-        </li>)}
+        </li>)} */}
+        {fake.map((el, i) => <li className={styles.list_item} key={i}>{el.name}</li>)}
     </ul>
   );
 };
