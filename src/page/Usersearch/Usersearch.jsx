12b2912504df05290a60d22959db1ca036b8cf65
Kooberium <kooberium@gmail.com>
2023-06-16 00:32:30 +0300

    Prevent flooding a search button

--- src/page/Usersearch/Usersearch.jsx
+++ src/page/Usersearch/Usersearch.jsx
@@ -10,28 +10,31 @@ import env from "react-dotenv";
 
 const Usersearch = () => {
   const form_ref = useRef(null)
-  const userrepos_container = useRef(null);
 
   const defaultStates = {
     input: { placeholder: "Введіть нікнейм користувача Github" },
     submit: { color: "rgb(47, 79, 79)", value: "Пошук" },
   };
 
-  const [submit_color, submit_setColor] = useState(
-    defaultStates.submit.color || "rgb(0,0,0)"
-  );
+  const [submit_color, submit_setColor] = useState(defaultStates.submit.color || "rgb(0,0,0)");
 
   const [usereposcontainer_opacity, set_usereposcontainer_opacity] = useState("0");
-  const [input_placeholder, set_input_placeholder] = useState(defaultStates.input.placeholder);
-  const [showrepos_btn, set_showrepos_btn] = useState(0);
-  const [user_repos, set_user_repos] = useState(null);
-  const [repos_searched, set_repos_searched] = useState(false);
 
+  const [input_placeholder, set_input_placeholder] = useState(defaultStates.input.placeholder);
+
+  const [showrepos_btn, set_showrepos_btn] = useState(0);
+
+  const [user_repos, set_user_repos] = useState(null);
   const [userdata, set_user_data] = useState(null || JSON.parse(localStorage.getItem('github_user_data')));
+
+  //////Система запобігання флудінню кнопкою пошуку
+  const [millis, set_millis] = useState(null);
+  const [flood_count, set_flood_count] = useState(0);
+  const [is_locked, set_lock] = useState(false);
 
 
   useEffect(() => {
-     fetchRepos();
+    fetchRepos();
   }, [userdata]);
 
   const searchUser = (username) => {
@@ -42,8 +45,7 @@ const Usersearch = () => {
 
     fetch(`${API}/${username}`).then((result) => {
       if (result.message && result.message === 'Not Found') {
-        set_input_placeholder('Користувача не було знайдено!');
-        submit_setColor("rgb(220, 55, 60)");
+        show_message('Користувача не було знайдено!', "rgb(220, 55, 60)")
         
         form_ref.current.reset()
         return
@@ -60,29 +62,35 @@ const Usersearch = () => {
   const onUserSearch = (e) => {
     e.preventDefault();
 
+    if (is_locked) {
+      show_message('Зачекайте хвильку!', 'rgb(220, 55, 60)')
+      return
+    } else {
+      handleFlooding();
+    }
+    
     if (!form_ref) return;
-
     const form = form_ref.current;
     
     const input = form.elements['controlpanel_input'];
     const input_value = input.value;
 
     if (userdata && userdata.login === input_value) {
-      submit_setColor("rgb(220, 150, 60)");
-      set_input_placeholder("Ви вже отримали дані цього користувача!");
+      show_message("Ви вже отримали дані цього користувача!", "rgb(220, 150, 60)")
+
       form.reset();
       return;
     }
 
     if (!input_value) {
-      submit_setColor("rgb(220, 150, 60)");
-      set_input_placeholder("Ви повинні вести нікнейм користувача");
+      show_message("Ви повинні вести нікнейм користувача", "rgb(220, 150, 60)")
+
       form.reset();
       return;
     }
 
     searchUser(input_value)
-    submit_setColor("rgb(0, 250, 154)");
+    submit_change_color("rgb(0, 250, 154)");
   };
 
   const onInputActivate = () => {
@@ -91,7 +99,7 @@ const Usersearch = () => {
   };
 
 
-   async function fetchRepos() {
+  async function fetchRepos() {
     if (!userdata) return;
     if (!userdata.repos_url) return;
     
@@ -111,6 +119,43 @@ const Usersearch = () => {
       set_usereposcontainer_opacity(1);
       
     }
+  };
+
+
+  const submit_change_color = (rgb) => {
+    if (!rgb) return;
+    if (typeof rgb !== 'string') return;
+
+    submit_setColor(rgb)
+
+    setTimeout(() => {
+      submit_setColor(defaultStates.submit.color)
+      set_input_placeholder(defaultStates.input.placeholder)
+    }, 2000);
+  };
+
+  const show_message = (text, rgb) => {
+    if (!text) return;
+    if (typeof text !== 'string') return;
+
+    if (!rgb) return;
+    if (typeof rgb !== 'string') return;
+
+
+    set_input_placeholder(text);
+    submit_change_color(rgb);
+  };
+
+  const handleFlooding = () => {
+    if (!is_locked && flood_count >= 5) {
+      set_lock(true);
+      setTimeout(set_lock, 5000, false);
+      return;
+    };
+
+    if (millis && Date.now() - millis < 1000) set_flood_count(flood_count + 1);
+
+    set_millis(Date.now());
   };
 
   return (
@@ -148,7 +193,6 @@ const Usersearch = () => {
 
             <div
               className={styles.user_repos}
-              ref={userrepos_container}
               style={{ opacity: usereposcontainer_opacity, display: `${showrepos_btn ? 'block' : 'none'}` }}
             >
               {user_repos && showrepos_btn ? (
